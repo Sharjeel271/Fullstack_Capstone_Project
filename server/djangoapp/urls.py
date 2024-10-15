65ac217a1d55f86506880014bc55aff7ecf7b573
Sharjeel <sharjeelmalik71@yahoo.com>
2024-10-15 07:42:08 -0400

    Added deale list, details and the ability to add reviews

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-
+    
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 
